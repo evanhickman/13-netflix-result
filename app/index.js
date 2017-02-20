f32b0f89466775ff1ef4799850099ae78b5adca6
Evan Hickman <travisevanhickman@gmail.com>
2017-02-20 17:02:02 -0600

    successfully run function

--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
 import 'whatwg-fetch';
+import fillInResult from './fill-in-result';
 
 const searchResult = document.querySelector('.result');
 const searchField = document.querySelector('.result-search__field');
 const searchBtn = document.querySelector('.result-search__button');
+
+fetch('http://netflixroulette.net/api/api.php?title=Attack%20on%20titan')
+  .then((res) => res.json())
+  .then((data) => {
+    fillInResult(searchResult, data);
+  });
