23dcbd7496d85f5a41180c77d6b86a548de387b4
Evan Hickman <travisevanhickman@gmail.com>
2017-02-20 16:18:10 -0600

    create variables

--- app/index.js
+++ app/index.js
@@ -1,1 +1,5 @@
 import 'whatwg-fetch';
+
+const searchResult = document.querySelector('.result');
+const searchField = document.querySelector('.result-search__field');
+const searchBtn = document.querySelector('.result-search__button');
