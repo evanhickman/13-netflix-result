01a1d0fc1eea9ad336b3ab6c526461bce7e29b37
Evan Hickman <travisevanhickman@gmail.com>
2017-02-23 19:03:57 -0600

    fix search

--- app/index.js
+++ app/index.js
@@ -1,19 +1,19 @@
 import 'whatwg-fetch';
 import fillInResult from './fill-in-result';
 
-const searchResult = document.querySelector('.result');
-const searchField = document.querySelector('.result-search__field');
 const searchBtn = document.querySelector('.result-search__button');
+const searchField = document.querySelector('.result-search__field');
+const searchResult = document.querySelector('.result');
 
-fetch('http://netflixroulette.net/api/api.php?title=Attack%20on%20titan')
+function startSearch(name) {
+  fetch(`http://netflixroulette.net/api/api.php?title=${name}`)
   .then((res) => res.json())
   .then((data) => {
     fillInResult(searchResult, data);
   });
-
-function fillResults(results) {
-  const resultData = document.querySelector('.result');
-  resultData.innerHTML = '';
-
-
 }
+
+searchBtn.addEventListener('click', () => {
+  startSearch(searchField.value);
+});
+startSearch('Attack on Titan');
